cab9cd4809efb30dffe526bb1523565cf6d5d18b
Miguel Decode <migueldecode@gmail.com>
2022-05-22 00:09:24 +0200

    First commit working in a responsive-personal-portfolio

---
+++ Mayo2022/responsive-personal-portfolio/js/main.js
@@ -0,0 +1,34 @@
+// MENU
+
+let menu = document.getElementById("toggle");
+let nav = document.getElementById("nav");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+// ANIMATION PRESENTATION
+
+const animationText = [
+  "aprender cosas nuevas.",
+  "resolver problemas.",
+  "trabajar en equipo.",
+];
+
+let i = 0;
+
+const roulette = () => {
+  if (i === animationText.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+  document.getElementById("roulette").innerHTML = animationText[i];
+};
+
+roulette();
+
+setInterval(() => {
+  roulette();
+}, 3500);
