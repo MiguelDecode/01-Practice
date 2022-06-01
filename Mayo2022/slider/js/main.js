8ffda43027b46461d8624fa96ead56188ef2b35c
Miguel Decode <migueldecode@gmail.com>
2022-06-01 19:43:58 +0200

    Finish slider project

--- Mayo2022/slider/js/main.js
+++ Mayo2022/slider/js/main.js
@@ -3,27 +3,54 @@ const slider = document.getElementById("slider");
 const buttonLeft = document.getElementById("left");
 const buttonRight = document.getElementById("right");
 
+const sliderImages = document.querySelectorAll(".slider__image");
+
 const rootStyles = document.documentElement.style;
+
+let slideCounter = 0;
+let isInTransition = false;
 
 const DIRECTION = {
   RIGHT: "RIGHT",
   LEFT: "LEFT",
 };
 
-const getTranformValue = () => Number(rootStyles.getPropertyValue('--slide-tranform').replace('%', ''));
+const getTranformValue = () =>
+  Number(rootStyles.getPropertyValue("--slide-transform").replace("%", ""));
 
-const moveSlide = direction => {
-
+const reorderSlide = () => {
   const transformValue = getTranformValue();
-
-  if(direction === DIRECTION.LEFT) {
-    rootStyles.setProperty('--slide-transform', `${transformValue + 100}%`)
-  } else if (direction === DIRECTION.RIGHT) {
-    rootStyles.setProperty('--slide-transform', `${transformValue - 100}%`)
+  rootStyles.setProperty("--transition", "none");
+  if (slideCounter === sliderImages.length - 1) {
+    slider.appendChild(slider.firstElementChild);
+    rootStyles.setProperty("--slide-transform", `${transformValue + 100}%`);
+    slideCounter--;
+  } else if (slideCounter === 0) {
+    slider.prepend(slider.lastElementChild);
+    rootStyles.setProperty("--slide-transform", `${transformValue - 100}%`);
+    slideCounter++;
   }
-}
+  isInTransition = false;
+};
+
+const moveSlide = (direction) => {
+  if(isInTransition) return
+  const transformValue = getTranformValue();
+  rootStyles.setProperty("--transition", "transform 1s");
+  isInTransition = true;
+
+  if (direction === DIRECTION.LEFT) {
+    rootStyles.setProperty("--slide-transform", `${transformValue + 100}%`);
+    slideCounter--;
+  } else if (direction === DIRECTION.RIGHT) {
+    rootStyles.setProperty("--slide-transform", `${transformValue - 100}%`);
+    slideCounter++;
+  }
+};
 
 buttonRight.addEventListener("click", () => moveSlide(DIRECTION.RIGHT));
 buttonLeft.addEventListener("click", () => moveSlide(DIRECTION.LEFT));
 
+slider.addEventListener("transitionend", reorderSlide);
 
+reorderSlide();
