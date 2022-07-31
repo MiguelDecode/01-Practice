989ada0118029307a191097be63c45ec762e5798
migueldecode <migueldecode@gmail.com>
2022-07-31 12:28:09 +0200

    Continue to study Javascript DOM

--- 07-2022/DOM/dom.js
+++ 07-2022/DOM/dom.js
@@ -118,4 +118,202 @@ $html.style.setProperty("--dark-color", "#333");
 varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
 $body.style.setProperty("background-color", varDarkColor); */
 
-// Clase 65 DOM: 
+// Clase 65 DOM: Classes con CSS
+
+/* const $card = document.querySelector(".card");
+
+console.log($card);
+console.log($card.className);
+console.log($card.classList);
+console.log($card.classList.contains("rotate-45"));
+$card.classList.add("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+console.log($card.classList);
+$card.classList.remove("rotate-45");
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+$card.classList.toggle("rotate-45");
+$card.classList.replace("rotate-45", "rotate-135");
+$card.classList.add("opacity-80", "sepia");
+$card.classList.remove("opacity-80", "sepia");
+$card.classList.toggle("opacity-80", "sepia"); */
+
+// Clase 66 DOM: Texto y HTML
+
+/* const $whatIsDOM = document.getElementById("que-es");
+
+let text = `
+<p>
+El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model</i></b>) es una API para documentos HTML y XML
+</p>
+<p>
+Este proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+</p>
+<p>
+<mark> El DOM no es parte de la especificación de JavaScript es una API para los navegadores.</mark>
+</p>`;
+
+$whatIsDOM.innerText = text; // No forma parte del estándar
+$whatIsDOM.textContent = text;
+$whatIsDOM.innerHTML = text;
+
+$whatIsDOM.outerHTML = text; */
+
+// Clase 67 DOM: Traversing - Recorriendo el DOM.
+
+/* const $cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children);
+console.log($cards.childNodes);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.firstChild);
+console.log($cards.firstElementChild);
+console.log($cards.lastChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousSibling);
+console.log($cards.previousElementSibling);
+console.log($cards.nextSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest('div')); // Método que busca el ancestro más cercano del tipo de selector indicado
+console.log($cards.children[3].closest('section')); */
+
+// Clase 68 DOM: Creando elementos y fragmentos.
+
+/* const $figure = document.createElement("figure");
+const $image = document.createElement("img");
+const $figcaption = document.createElement("figcaption");
+const $figcaptionText = document.createTextNode("Animals");
+const $cards = document.querySelector(".cards");
+const $figure2 = document.createElement("figure");
+
+$image.setAttribute("src", "https://placeimg.com/200/200/animals");
+$image.setAttribute("alt", "Animals");
+$figure.classList.add("card");
+
+$figcaption.appendChild($figcaptionText);
+$figure.appendChild($image);
+$figure.appendChild($figcaption);
+$cards.appendChild($figure);
+
+$figure2.innerHTML = `
+<img src='https://placeimg.com/200/200/people' alt='People'>
+<figcaption>People</figcaption>
+`;
+
+$figure2.classList.add("card");
+$cards.appendChild($figure2);
+
+const seasons = ["Primavera", "Verano", "Otoño", "Invierno"];
+const $ul = document.createElement("ul");
+
+document.write("<h3>Seasons of the year</h3>");
+document.body.appendChild($ul);
+
+seasons.forEach((n) => {
+  const $li = document.createElement("li");
+  $li.textContent = n;
+  $ul.appendChild($li);
+});
+
+const continents = ["Africa", "América", "Asia", "Europa", "Oceanía"];
+const $ul2 = document.createElement("ul");
+
+document.write("<h3>Continents of the World</h3>");
+document.body.appendChild($ul2);
+$ul2.innerHTML = "";
+continents.forEach((n) => ($ul2.innerHTML += `<li>${n}</li>`));
+
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+$ul3 = document.createElement("ul");
+$fragment = document.createDocumentFragment();
+
+months.forEach((n) => {
+  const $li = document.createElement("li");
+  $li.textContent = n;
+  $fragment.appendChild($li);
+});
+
+document.write("<h3>Months of the Year</h3>");
+$ul3.appendChild($fragment);
+document.body.appendChild($ul3); */
+
+// Clase 69 DOM: Templates HTML
+
+/* const $cards = document.querySelector(".cards");
+const $template = document.getElementById("template-card").content;
+const $fragment = document.createDocumentFragment();
+const cardContent = [
+  {
+    title: "Tecnología",
+    img: "https://placeimg.com/200/200/tech",
+  },
+  {
+    title: "Animales",
+    img: "https://placeimg.com/200/200/animals",
+  },
+  {
+    title: "Arquitectura",
+    img: "https://placeimg.com/200/200/arch",
+  },
+  {
+    title: "Gente",
+    img: "https://placeimg.com/200/200/people",
+  },
+  {
+    title: "Naturaleza",
+    img: "https://placeimg.com/200/200/nature ",
+  },
+];
+
+cardContent.forEach((n) => {
+  $template.querySelector("img").setAttribute("src", n.img);
+  $template.querySelector("img").setAttribute("alt", n.title);
+  $template.querySelector("figcaption").textContent = n.title;
+
+  let $clone = document.importNode($template, true);
+  $fragment.appendChild($clone);
+});
+
+$cards.appendChild($fragment); */
+
+// Clase 70 DOM: Modificando Elementos(Old Style)
+
+/* const $cards = document.querySelector(".cards");
+const $newCard = document.createElement("figure");
+const $cloneCards = $cards.cloneNode(true);
+
+$newCard.innerHTML = `
+<img src='https://placeimg.com/200/200/any' alt='Any'>
+<figcaption>Any</figcaption>
+`;
+
+$newCard.classList.add("card");
+
+// $cards.replaceChild($newCard, $cards.children[2]);
+
+// $cards.insertBefore($newCard, $cards.firstElementChild);
+
+// $cards.removeChild($cards.lastElementChild);
+
+document.body.appendChild($cloneCards); */
+
+// Clase 71 DOM: Modificando Elementos (Cool Style)
+
+
