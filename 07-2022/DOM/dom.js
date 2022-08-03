2f7e789c13a0b1a5db64a156930fd34363ed4785
migueldecode <migueldecode@gmail.com>
2022-08-03 19:11:10 +0200

    Continue to study Javascript BOM

--- 07-2022/DOM/dom.js
+++ 07-2022/DOM/dom.js
@@ -397,4 +397,171 @@ $eventoRemover.addEventListener("dblclick", removerDobleClick); */
 
 // Clase 74 DOM: Flujo de Eventos (Burbuja y Captura)
 
+/* const $divsEventos = document.querySelectorAll(".eventos-flujo div");
 
+console.log($divsEventos);
+
+function flujoEventos(e) {
+  console.log(
+    `Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
+  );
+}
+
+$divsEventos.forEach((div) => {
+  div.addEventListener("click", flujoEventos);
+  // Fase de burbuja
+  div.addEventListener("click", flujoEventos, false);
+  // Fase de captura
+  div.addEventListener("click", flujoEventos, true);
+  // Otros valores
+  div.addEventListener("click", flujoEventos, {
+    capture: true,
+  });
+  div.addEventListener("click", flujoEventos, {
+    capture: false,
+    once: true,
+  });
+}); */
+
+// Clase 75 DOM: stopPropagation & preventDefault
+
+// const $divsEventos = document.querySelectorAll(".eventos-flujo div");
+// const $linkEventos = document.querySelector(".eventos-flujo a");
+
+// console.log($divsEventos);
+
+// function flujoEventos(e) {
+//   console.log(
+//     `Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
+//   );
+//   e.stopPropagation(); // Método para detener la propagación.
+// }
+
+// $divsEventos.forEach((div) => {
+//   div.addEventListener("click", flujoEventos);
+//   // // Fase de burbuja
+//   // div.addEventListener("click", flujoEventos, false);
+//   // // Fase de captura
+//   // div.addEventListener("click", flujoEventos, true);
+//   // // Otros valores
+//   // div.addEventListener("click", flujoEventos, {
+//   //   capture: true,
+//   // });
+//   // div.addEventListener("click", flujoEventos, {
+//   //   capture: false,
+//   //   once: true,
+//   // });
+// });
+
+// $linkEventos.addEventListener("click", (event) => {
+//   alert("Hola soy tu amigo y docente digital Jonathan Mircha");
+//   event.preventDefault();
+//   event.stopPropagation();
+// });
+
+// Clase 76 DOM: Delegación de Eventos
+
+/* function flujoEventos(event) {
+  console.log(
+    `Hola te saluda ${this.className}, el click lo originó ${event.target.className}`
+  );
+}
+
+document.addEventListener("click", (event) => {
+  console.log(`Click en ${event.target}`);
+
+  if (event.target.matches("eventos-flujo div")) {
+    flujoEventos(event);
+  }
+
+  if (event.target.matches(".eventos-flujo a")) {
+    alert("Hola soy tu amigo y docente digital Jonathan Mircha");
+    event.preventDefault();
+  }
+}); */
+
+// Clase 77 BOM: Propiedades y Eventos
+
+/* window.addEventListener("resize", (event) => {
+  console.clear();
+  console.log("Evento Resize");
+  console.log(window.innerWidth);
+  console.log(window.innerHeight);
+  console.log(window.outerWidth);
+  console.log(window.outerHeight);
+  console.log(event);
+});
+
+window.addEventListener("scroll", (event) => {
+  console.clear();
+  console.log("Evento Scroll");
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(event);
+});
+
+window.addEventListener("load", (event) => {
+  console.clear();
+  console.log("Evento Load");
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(event);
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("Evento DOMContentLoaded");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(event);
+}); */
+
+// Clase 78 BOM: Métodos
+
+/* const $btnAbrir = document.getElementById("abrir-ventana");
+const $btnCerrar = document.getElementById("cerrar-ventana");
+const $btnImprimir = document.getElementById("imprimir-ventana");
+
+let ventana;
+
+$btnAbrir.addEventListener("click", (event) => {
+  ventana = window.open('https://jonmircha.com')
+});
+
+$btnCerrar.addEventListener("click", (event) => {
+  //window.close();
+  ventana.close();
+});
+
+$btnImprimir.addEventListener("click", (event) => {
+  window.print();
+}); */
+
+// Clase 79 BOM: Objetos: URL, Historial y Navegador
+
+/* console.log(location);
+console.log(location.origin); // Muestra el origen de la pagina https://127.0.0.1:5500
+console.log(location.protocol); // Muestra el protocolo usado
+console.log(location.host); // Muestra el nombre del dominio
+console.log(location.hostname); // Muestra el nombre del dominio
+console.log(location.port); // Muestra el puerto
+console.log(location.href); // Muestra la dirección url al completo
+console.log(location.hash); // Muestra el valor de url después de un hash
+console.log(location.search); // Muestra los valores despues de un interrogante
+console.log(location.pathname); */
+
+/* console.log(history);
+console.log(history.length); // Muestra el total de páginas entre las que se ha navegado
+console.log(history.back(3)); // Retorna una cantidad indicada de páginas hacia atrás
+console.log(history.forward(3)); // Avanza una cantidad indicada de páginas hacia delante
+console.log(history.go()); // Avanza o retorna una cantidad indicada de páginas según el signo y el número aportado */
+
+/* console.log(navigator);
+console.log(navigator.connection); // Nos da información de la conexión del usuario
+console.log(navigator.geolocation); // Nos da información sobre la geolocalización
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes); // Los tipos de formatos que acepta el navegador
+console.log(navigator.onLine); // Nos da información sobre el estado de la conexión
+console.log(navigator.serviceWorker.Worker); // Un API que ayuda a hacer Progressive Web Apps
+console.log(navigator.storage); // API de almacenamiento
+console.log(navigator.usb); // Detectar dispositivos USB
+console.log(navigator.userAgent); // Nos da información sobre el sistema operativo del usuario */
