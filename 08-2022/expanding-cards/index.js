16a249f60ad8aca523bd375eb24285e81152cb06
Miguel Decode <migueldecode@gmail.com>
2022-08-20 17:04:54 +0200

    Improvement: Try another javascript method for detect event and reapair overflow h1

--- 08-2022/expanding-cards/index.js
+++ 08-2022/expanding-cards/index.js
@@ -4,9 +4,17 @@ function removeActive() {
   flexItems.forEach((n) => n.classList.remove("active"));
 }
 
-flexItems.forEach((n) =>
+/* flexItems.forEach((n) =>
   n.addEventListener("click", () => {
     removeActive();
     n.classList.add("active");
   })
-);
+); */
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList[0] === 'flex-item') {
+    removeActive();
+    event.target.classList.add('active')
+  }
+});
+
