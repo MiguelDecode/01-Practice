cb48ee26cfb5ea87addb4ff0242404ebfef88dfe
Miguel Decode <migueldecode@gmail.com>
2022-09-01 22:18:34 +0200

    Add exercise javascript calculator

--- 08-2022/calculator-design/index.js
+++ 08-2022/calculator-design/index.js
@@ -8,16 +8,17 @@ const lastValue = () =>
   screenOperation.textContent.substring(screenOperation.textContent.length - 1);
 
 const writeOperation = (element) => {
-  if (screenOperation.textContent == 0) screenOperation.textContent = "";
+  if (screenOperation.textContent == "0" && element != ".")
+    screenOperation.textContent = "";
 
-  if(operationComplete && isNaN(element)) {
+  if (operationComplete && isNaN(element)) {
     screenOperation.textContent = screenResult.textContent;
     operationComplete = false;
   }
 
-  if(operationComplete && !isNaN(element)) {
-    screenOperation.textContent = '';
-    screenResult.textContent = '0';
+  if (operationComplete && !isNaN(element)) {
+    screenOperation.textContent = "";
+    screenResult.textContent = "0";
     operationComplete = false;
   }
 
@@ -32,14 +33,45 @@ const writeOperation = (element) => {
 };
 
 const writeResult = () => {
+  if (isNaN(lastValue()) && lastValue() !== ")")
+    screenOperation.textContent = screenOperation.textContent.substring(
+      0,
+      screenOperation.textContent.length - 1
+    );
+
   screenResult.textContent = eval(screenOperation.textContent);
   operationComplete = true;
+
+  if (screenResult.textContent.length > 9) {
+    screenResult.style.fontSize = "2em";
+    screenResult.style.marginTop = "1em";
+  }
+};
+
+const changeSign = () => {
+  let lastNumber = "";
+  let position = 0;
+
+  if (!isNaN(lastValue())) {
+    for (let i = screenOperation.textContent.length - 1; i > 0; i--) {
+      if (isNaN(screenOperation.textContent[i])) {
+        position = i + 1;
+        break;
+      }
+    }
+  }
+
+  lastNumber = screenOperation.textContent.substring(position);
+  screenOperation.textContent = screenOperation.textContent.replace(
+    lastNumber,
+    `(${lastNumber * -1})`
+  );
 };
 
 const resetScreen = () => {
-  screenOperation.textContent = '0';
-  screenResult.textContent = '0';
-}
+  screenOperation.textContent = "0";
+  screenResult.textContent = "0";
+};
 
 buttons.addEventListener("click", (event) => {
   if (event.target.textContent !== "") {
