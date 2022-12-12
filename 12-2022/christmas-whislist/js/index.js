a38a88ccb1edbcdc1a40bd198f9e3b2d85e9a89e
migueldecode <migueldecode@gmail.com>
2022-12-12 22:51:51 +0100

    Work in my Chirstmas Whislist card with music

--- 12-2022/christmas-whislist/js/index.js
+++ 12-2022/christmas-whislist/js/index.js
@@ -1,14 +1,16 @@
 const book = document.getElementById("book");
 const song = document.getElementById("song");
 const whishlist = document.getElementById("whislist");
-const input = document.getElementById("whish");
+const input = document.getElementById("whishes");
+const body = document.querySelector(".body");
 
 input.addEventListener("keyup", (event) => {
-  if(event.code === 'Enter') {
+  if (event.code === "Enter") {
     const $li = document.createElement("li");
     $li.textContent = input.value;
+    $li.classList.add("whishlist__item");
     whishlist.appendChild($li);
-    input.value = '';
+    input.value = "";
   }
 });
 
@@ -19,3 +21,32 @@ book.addEventListener("mouseenter", () => {
 book.addEventListener("mouseleave", () => {
   song.pause();
 });
+
+// Snow effect
+
+const getRandomValue = (max, min= 1) => {
+return Math.floor(Math.random() * max) + min; 
+}
+
+const createSnow = (density) => {
+  for (let i = 0; i < density; i++) {
+    const snowFlake = document.createElement("span");
+
+    const randomHorizontalPosition = `${getRandomValue(100)}%`;
+    const randomFallDelay = `${getRandomValue(100)}s`;
+    const randomFallDuration = `${getRandomValue(20,5)}s`;
+    const randomFlakeSize = `${getRandomValue(7,1)}px`;
+    const randomFlakeOpacity = Math.random().toFixed(2);
+
+    snowFlake.style.width = randomFlakeSize;
+    snowFlake.style.height = randomFlakeSize;
+    snowFlake.style.opacity = randomFlakeOpacity;
+    snowFlake.style.right = randomHorizontalPosition;
+    snowFlake.style.animation = `fall ${randomFallDuration} ${randomFallDelay} linear infinite`;
+    snowFlake.classList.add("snow");
+
+    body.appendChild(snowFlake);
+  }
+};
+
+createSnow(300);
