6e0ad06691129945559156f6f06994e9cb8dd04f
migueldecode <migueldecode@gmail.com>
2022-12-24 16:46:59 +0100

    Finish my case of study of a parallax element

--- 12-2022/responsive-christmas-website-2-main/assets/js/main.js
+++ 12-2022/responsive-christmas-website-2-main/assets/js/main.js
@@ -35,11 +35,62 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the snow-scroll class to the tag with the scrollup
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== PARALLAX ===============*/
+let parallax = new Rellax(".parallax");
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__village", 1.2, { opacity: 0, y: 100, delay: 0.1 });
+gsap.from(".home__pine", 1.2, { opacity: 0, y: 150, delay: 0.3 });
+gsap.from(".home__mountain-2", 1.2, { opacity: 0, y: 150, delay: 0.5 });
+gsap.from(".home__mountain-3", 1.2, { opacity: 0, y: -150, delay: 0.6 });
+gsap.from(".home__mountain-1", 1.2, { opacity: 0, y: 250, delay: 0.7 });
+gsap.from(".home__moon", 1.2, { opacity: 0, y: 200, delay: 0.8 });
+gsap.from(".home__trineo", 1.2, { opacity: 0, x: -200, delay: 0.8 });
+gsap.from(".home__title", 1.2, { opacity: 0, y: -60, delay: 1 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "3.75rem",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(`.about__data, .celebrate__img`, { origin: "right" });
+sr.reveal(`.about__img, .celebrate__data`, { origin: "left" });
+sr.reveal(`.send__card`, { interval: 100 });
+sr.reveal(`.footer`);
